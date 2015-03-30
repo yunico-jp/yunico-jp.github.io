18794189e542f3ebfc6ee29b0c12e0e0eb39cf56
yunico <kaori.nagata@nanapi.co.jp>
2015-03-30 15:01:27 +0900

    modify! READMORE.

--- config.rb
+++ config.rb
@@ -16,8 +16,8 @@ activate :blog do |blog|
   # blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
   # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
+  blog.summary_separator = /READMORE/
+  blog.summary_length = nil
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
